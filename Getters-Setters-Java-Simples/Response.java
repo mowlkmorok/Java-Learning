76ee882391826884c0e3b2356cd6f95fe153fb67
mowlkmorok <65051191+mowlkmorok@users.noreply.github.com>
2021-04-25 06:41:58 -0400

    Create Response.java

---
+++ Getters-Setters-Java-Simples/Response.java
@@ -0,0 +1,16 @@
+package ui;
+
+import com.packege1.SumSimple;        //Import Class
+
+
+public class Response {
+    
+   private static void main(String[] args){
+       
+       SumSimple sum = new SumSimple();
+       sum.setSum(33, 40);
+       System.out.println(sum.getResult());
+       
+   } 
+   
+}
